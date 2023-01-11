912b20216348c5c2358687ac26bdefc832ac2396
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2023-01-11 16:56:31 -0500

    added password hashing

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,18 +1,40 @@
+// imports
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
+// schema
 const schema = new mongoose.Schema({
-    username: { type: String, unique: true },
+    // properties
+    username: { type: String, unique: true }, // primary key
     email: { type: String, unique: true, lowercase: true },
     password: { type: String },
 
     name: { type: String },
     picture: { type: String },
 
+    // pointer to weights object
     weights: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Weights"
     }
+});
+
+// hash a new password whenever password changed and saved
+schema.pre('save', async (next) => {
+
+    if(!this.isModified('password')) return next();
+
+    // hash w/ bcrypt
+    const hashedPassword = bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            console.log(err);
+        });
+    });
+
+    // set as password in db
+    this.password = hashedPassword;
+
+    return next();
 });
 
 const UserModel = mongoose.model('User', schema);
