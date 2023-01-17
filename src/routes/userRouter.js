16e50e5f9c9757a8b20fc703f9907bfcd0a40b0e
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2023-01-17 12:57:09 -0500

    added routes for user

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+router.post('/login', (req, res) => {
+
+});
+
+router.post('/register', (req, res) => {
+    
+});
+
 module.exports = router;
+
 
 
 
